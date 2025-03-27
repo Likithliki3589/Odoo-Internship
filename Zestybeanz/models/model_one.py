3faa859255d427ad370bc388f428996efe721803
likith_m <likithliki3589@gmail.com>
2025-03-27 20:02:58 +0530

    views and security

--- Zestybeanz/models/model_one.py
+++ Zestybeanz/models/model_one.py
@@ -1,17 +1,6 @@
-from odoo import models,fields
+from odoo import models, fields
 
-class modelOne(models.Model):
-   _name = "model.one"
-   _description = "Model One"
-
-#fields
-name = fields.Char(string="Name")
-price = fields.Float(string="Standard price")
-quantity = fields.Integer(string="Amoumt")
-review = fields.Text(string="Review of The Model one")
-is_Satisfied = fields.Boolan(string="satisfied/Not")
-Check_in = fields.Data(sting="Check In")
-served_time = fields.Datetime(string="Served Time")
-types = fields.Selection([('dine_in','Dine In'),('delivery','Delivery')])
-images = fields.Binary(string="Images")
-blog = fields.Html(string="Blog")
+class ModelOne(models.Model):
+	
+	_name = "model.one"
+	_description = "Model One"
